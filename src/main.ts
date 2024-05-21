85de1aaee9f2de23c5f8b6a7337120d818b0295d
tensingn <tensingn@oregonstate.edu>
2024-05-21 07:10:36 -0400

    increasing request size

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(json({ limit: '500kb' }));
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
     await app.listen(8080);
